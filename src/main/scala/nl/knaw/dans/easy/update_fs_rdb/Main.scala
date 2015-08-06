6ef74eed591a903e1f1dd43e3555b1cc1f6d01a0
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-08-06 17:37:35 +0200

    serialize easy-folder's + refactoring

--- src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
+++ src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
@@ -4,7 +4,7 @@ import com.yourmediashelf.fedora.client.request.FedoraRequest
 import com.yourmediashelf.fedora.client.{FedoraClient, FedoraCredentials}
 import org.slf4j.LoggerFactory
 
-import scala.util.Try
+import scala.util.{Failure, Try}
 import scala.xml.{Elem, XML}
 import scalaj.http.Http
 
@@ -19,36 +19,38 @@ object Main {
 
     implicit val s = Settings(
       fedoraCredentials = new FedoraCredentials("http://deasy:8080/fedora", "fedoraAdmin", "fedoraAdmin"),
-      pid = "easy-dataset:39")
+      datasetPid = "easy-dataset:39")
 
     FedoraRequest.setDefaultClient(new FedoraClient(s.fedoraCredentials))
 
     val result = for {
       pids <- findPids()
       _ = pids.foreach(log.info)
-      xs <- getDigitalObjects(pids)
-    } yield println(xs.head)
+      xs <- getDigitalObjects(pids).sequence
+    } yield xs.foreach(println)
 
     result.get
   }
 
-  def getDigitalObjects(pids: List[String])(implicit s: Settings): Try[List[DigitalObject]] = Try {
+  def getDigitalObjects(pids: List[String])(implicit s: Settings): List[Try[DigitalObject]] = {
     pids.map(pid =>
       if (pid.startsWith(NS_EASY_FILE)) {
-        val objectXML = XML.load(FedoraClient.getObjectXML(pid).execute().getEntityInputStream)
-        getEasyFile(pid, objectXML)
+        getObjectXML(pid).flatMap(getEasyFile(pid))
       }
       else if (pid.startsWith(NS_EASY_FOLDER)) {
-        val xml = XML.load(FedoraClient.getDatastream(pid, "EASY_ITEM_CONTAINER_MD").execute().getEntityInputStream)
-        getEasyFolder(pid, xml)
+        getObjectXML(pid).flatMap(getEasyFolder(pid))
       }
       else {
-        throw new RuntimeException(s"Unknown PID: $pid")
+        Failure(new RuntimeException(s"Unknown namespace for PID: $pid"))
       }
     )
   }
 
-  def getEasyFile(pid: String, objectXML: Elem)(implicit s: Settings): EasyFile = {
+  def getObjectXML(pid: String): Try[Elem] = Try {
+    XML.load(FedoraClient.getObjectXML(pid).execute().getEntityInputStream)
+  }
+
+  def getEasyFile(pid: String)(objectXML: Elem)(implicit s: Settings): Try[EasyFile] = Try {
     val result = for {
       metadataDS <- objectXML \ "datastream"
       if (metadataDS \ "@ID").text == "EASY_FILE_METADATA"
@@ -64,8 +66,8 @@ object Main {
       parentSid = isMemberOf.attribute("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "resource").get
     } yield EasyFile(
         pid = pid,
-        parentSid = parentSid.text,
-        datasetSid = s.pid,
+        parentSid = parentSid.text.replace("info:fedora/", ""),
+        datasetSid = s.datasetPid,
         path = (metadata \ "path").text,
         filename = (metadata \ "name").text,
         size = (metadata \ "size").text.toInt,
@@ -75,18 +77,29 @@ object Main {
         accessibleTo = (metadata \ "accessibleTo").text,
         sha1checksum = digest.text)
     if (result.size != 1)
-      throw new RuntimeException(s"Inconsistent digital object, please inspect $pid manually.")
+      throw new RuntimeException(s"Inconsistent file digital object, please inspect $pid manually.")
     result.head
   }
 
-  def getEasyFolder(pid: String, xml: Elem)(implicit s: Settings): EasyFolder = {
-    EasyFolder(
-      pid = pid,
-      parentSid = "",
-      datasetSid = s.pid,
-      path = "",
-      name = ""
-    )
+  def getEasyFolder(pid: String)(objectXML: Elem)(implicit s: Settings): Try[EasyFolder] = Try {
+    val result = for {
+      metadataDS <- objectXML \ "datastream"
+      if (metadataDS \ "@ID").text == "EASY_ITEM_CONTAINER_MD"
+      metadata <- metadataDS \ "datastreamVersion" \ "xmlContent" \ "item-container-md"
+
+      relsExtDS <- objectXML \ "datastream"
+      if (relsExtDS \ "@ID").text == "RELS-EXT"
+      isMemberOf <- relsExtDS \ "datastreamVersion" \ "xmlContent" \ "RDF" \ "Description" \ "isMemberOf"
+      parentSid = isMemberOf.attribute("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "resource").get
+    } yield EasyFolder(
+        pid = pid,
+        parentSid = parentSid.text.replace("info:fedora/", ""),
+        datasetSid = s.datasetPid,
+        path = (metadata \ "path").text,
+        name = (metadata \ "name").text)
+    if (result.size != 1)
+      throw new RuntimeException(s"Inconsistent folder digital object, please inspect $pid manually.")
+    result.head
   }
 
   def findPids()(implicit s: Settings): Try[List[String]] = Try {
@@ -95,7 +108,7 @@ object Main {
       .param("type", "tuples")
       .param("lang", "sparql")
       .param("format", "CSV")
-      .param("query", s"select ?s from <#ri> where { ?s <http://dans.knaw.nl/ontologies/relations#isSubordinateTo> <info:fedora/${s.pid}> . }")
+      .param("query", s"select ?s from <#ri> where { ?s <http://dans.knaw.nl/ontologies/relations#isSubordinateTo> <info:fedora/${s.datasetPid}> . }")
       .asString
     if (response.code != 200)
       throw new RuntimeException(s"Failed to query fedora resource index ($url), response code: ${response.code}")
--- src/main/scala/nl/knaw/dans/easy/update_fs_rdb/package.scala
+++ src/main/scala/nl/knaw/dans/easy/update_fs_rdb/package.scala
@@ -2,10 +2,12 @@ package nl.knaw.dans.easy
 
 import com.yourmediashelf.fedora.client.FedoraCredentials
 
+import scala.util.{Success, Failure, Try}
+
 package object update_fs_rdb {
 
   case class Settings(fedoraCredentials: FedoraCredentials,
-                      pid: String)
+                      datasetPid: String)
 
   abstract class DigitalObject(pid: String,
                                parentSid: String,
@@ -30,4 +32,13 @@ package object update_fs_rdb {
                       accessibleTo: String,
                       sha1checksum: String) extends DigitalObject(pid, parentSid, datasetSid, path)
 
+  class CompositeException(throwables: List[Throwable]) extends RuntimeException(throwables.foldLeft("")((msg, t) => s"$msg\n${t.getMessage}"))
+
+  implicit class ListTryExtensions[T](xs: List[Try[T]]) {
+    def sequence: Try[List[T]] =
+      if (xs.exists(_.isFailure))
+        Failure(new CompositeException(xs.collect { case Failure(e) => e }))
+      else
+        Success(xs.map(_.get))
+  }
 }
