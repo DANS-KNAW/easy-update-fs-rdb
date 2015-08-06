91aaa30160173a455c644dcdd86c002a14d9bccb
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-08-06 17:01:16 +0200

    Deserialize easy-file's

--- src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
+++ src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
@@ -1,54 +1,96 @@
 package nl.knaw.dans.easy.update_fs_rdb
 
+import com.yourmediashelf.fedora.client.request.FedoraRequest
+import com.yourmediashelf.fedora.client.{FedoraClient, FedoraCredentials}
 import org.slf4j.LoggerFactory
 
 import scala.util.Try
+import scala.xml.{Elem, XML}
 import scalaj.http.Http
 
 object Main {
   val log = LoggerFactory.getLogger(getClass)
 
-  case class Settings(fedoraURL: String, pid: String, namespaces: List[String])
-
-  abstract class DigitalObject(pid: String,
-                               parentSid: String,
-                               datasetSid: String,
-                               path: String)
-
-  case class EasyFolder(pid: String,
-                        parentSid: String,
-                        datasetSid: String,
-                        path: String,
-                        name: String) extends DigitalObject(pid, parentSid, datasetSid, path)
-
-  case class EasyFile(pid: String,
-                      parentSid: String,
-                      datasetSid: String,
-                      path: String,
-                      filename: String,
-                      size: Int,
-                      mimetype: String,
-                      creatorRole: String,
-                      visibleTo: String,
-                      accessibleTo: String,
-                      sha1checksum: String) extends DigitalObject(pid, parentSid, datasetSid, path)
+  val NS_EASY_FILE = "easy-file"
+  val NS_EASY_FOLDER = "easy-folder"
+  val namespaces = List(NS_EASY_FILE, NS_EASY_FOLDER)
 
   def main(args: Array[String]) {
 
     implicit val s = Settings(
-      fedoraURL = "http://deasy:8080/fedora",
-      pid = "easy-dataset:39",
-      namespaces = List("easy-folder", "easy-file"))
+      fedoraCredentials = new FedoraCredentials("http://deasy:8080/fedora", "fedoraAdmin", "fedoraAdmin"),
+      pid = "easy-dataset:39")
+
+    FedoraRequest.setDefaultClient(new FedoraClient(s.fedoraCredentials))
 
     val result = for {
       pids <- findPids()
-    } yield pids.foreach(log.info)
+      _ = pids.foreach(log.info)
+      xs <- getDigitalObjects(pids)
+    } yield println(xs.head)
 
     result.get
   }
 
+  def getDigitalObjects(pids: List[String])(implicit s: Settings): Try[List[DigitalObject]] = Try {
+    pids.map(pid =>
+      if (pid.startsWith(NS_EASY_FILE)) {
+        val objectXML = XML.load(FedoraClient.getObjectXML(pid).execute().getEntityInputStream)
+        getEasyFile(pid, objectXML)
+      }
+      else if (pid.startsWith(NS_EASY_FOLDER)) {
+        val xml = XML.load(FedoraClient.getDatastream(pid, "EASY_ITEM_CONTAINER_MD").execute().getEntityInputStream)
+        getEasyFolder(pid, xml)
+      }
+      else {
+        throw new RuntimeException(s"Unknown PID: $pid")
+      }
+    )
+  }
+
+  def getEasyFile(pid: String, objectXML: Elem)(implicit s: Settings): EasyFile = {
+    val result = for {
+      metadataDS <- objectXML \ "datastream"
+      if (metadataDS \ "@ID").text == "EASY_FILE_METADATA"
+      metadata <- metadataDS \ "datastreamVersion" \ "xmlContent" \ "file-item-md"
+
+      fileDS <- objectXML \ "datastream"
+      if (fileDS \ "@ID").text == "EASY_FILE"
+      digest <- fileDS \ "datastreamVersion" \ "contentDigest" \ "@DIGEST"
+
+      relsExtDS <- objectXML \ "datastream"
+      if (relsExtDS \ "@ID").text == "RELS-EXT"
+      isMemberOf <- relsExtDS \ "datastreamVersion" \ "xmlContent" \ "RDF" \ "Description" \ "isMemberOf"
+      parentSid = isMemberOf.attribute("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "resource").get
+    } yield EasyFile(
+        pid = pid,
+        parentSid = parentSid.text,
+        datasetSid = s.pid,
+        path = (metadata \ "path").text,
+        filename = (metadata \ "name").text,
+        size = (metadata \ "size").text.toInt,
+        mimetype = (metadata \ "mimeType").text,
+        creatorRole = (metadata \ "creatorRole").text,
+        visibleTo = (metadata \ "visibleTo").text,
+        accessibleTo = (metadata \ "accessibleTo").text,
+        sha1checksum = digest.text)
+    if (result.size != 1)
+      throw new RuntimeException(s"Inconsistent digital object, please inspect $pid manually.")
+    result.head
+  }
+
+  def getEasyFolder(pid: String, xml: Elem)(implicit s: Settings): EasyFolder = {
+    EasyFolder(
+      pid = pid,
+      parentSid = "",
+      datasetSid = s.pid,
+      path = "",
+      name = ""
+    )
+  }
+
   def findPids()(implicit s: Settings): Try[List[String]] = Try {
-    val url = s"${s.fedoraURL}/risearch"
+    val url = s"${s.fedoraCredentials.getBaseUrl}/risearch"
     val response = Http(url)
       .param("type", "tuples")
       .param("lang", "sparql")
@@ -59,6 +101,6 @@ object Main {
       throw new RuntimeException(s"Failed to query fedora resource index ($url), response code: ${response.code}")
     response.body.lines.toList.drop(1)
       .map(_.replace("info:fedora/", ""))
-      .filter(pid => s.namespaces.exists(pid.startsWith))
+      .filter(pid => namespaces.exists(pid.startsWith))
   }
 }
