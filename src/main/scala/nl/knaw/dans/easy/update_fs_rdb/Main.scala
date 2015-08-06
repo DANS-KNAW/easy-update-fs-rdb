5a728c94489a9f84c6d7c6fc122edef6a4312ffd
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-08-06 10:58:00 +0200

    data objects + extracted method for querying RI

--- src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
+++ src/main/scala/nl/knaw/dans/easy/update_fs_rdb/Main.scala
@@ -2,26 +2,63 @@ package nl.knaw.dans.easy.update_fs_rdb
 
 import org.slf4j.LoggerFactory
 
+import scala.util.Try
 import scalaj.http.Http
 
 object Main {
   val log = LoggerFactory.getLogger(getClass)
 
+  case class Settings(fedoraURL: String, pid: String, namespaces: List[String])
+
+  abstract class DigitalObject(pid: String,
+                               parentSid: String,
+                               datasetSid: String,
+                               path: String)
+
+  case class EasyFolder(pid: String,
+                        parentSid: String,
+                        datasetSid: String,
+                        path: String,
+                        name: String) extends DigitalObject(pid, parentSid, datasetSid, path)
+
+  case class EasyFile(pid: String,
+                      parentSid: String,
+                      datasetSid: String,
+                      path: String,
+                      filename: String,
+                      size: Int,
+                      mimetype: String,
+                      creatorRole: String,
+                      visibleTo: String,
+                      accessibleTo: String,
+                      sha1checksum: String) extends DigitalObject(pid, parentSid, datasetSid, path)
+
   def main(args: Array[String]) {
 
-    val fedoraURL = "http://deasy:8080/fedora"
-    val pid = "easy-dataset:39"
-    val namespaces = List("easy-folder", "easy-file")
+    implicit val s = Settings(
+      fedoraURL = "http://deasy:8080/fedora",
+      pid = "easy-dataset:39",
+      namespaces = List("easy-folder", "easy-file"))
 
-    val response = Http(s"$fedoraURL/risearch")
+    val result = for {
+      pids <- findPids()
+    } yield pids.foreach(log.info)
+
+    result.get
+  }
+
+  def findPids()(implicit s: Settings): Try[List[String]] = Try {
+    val url = s"${s.fedoraURL}/risearch"
+    val response = Http(url)
       .param("type", "tuples")
       .param("lang", "sparql")
       .param("format", "CSV")
-      .param("query", s"select ?s from <#ri> where { ?s <http://dans.knaw.nl/ontologies/relations#isSubordinateTo> <info:fedora/$pid> . }")
+      .param("query", s"select ?s from <#ri> where { ?s <http://dans.knaw.nl/ontologies/relations#isSubordinateTo> <info:fedora/${s.pid}> . }")
       .asString
-
-    val pids = response.body.lines.drop(1).map(_.replace("info:fedora/", "")).filter(pid => namespaces.exists(pid.startsWith))
-
-    pids.foreach(println)
+    if (response.code != 200)
+      throw new RuntimeException(s"Failed to query fedora resource index ($url), response code: ${response.code}")
+    response.body.lines.toList.drop(1)
+      .map(_.replace("info:fedora/", ""))
+      .filter(pid => s.namespaces.exists(pid.startsWith))
   }
 }
