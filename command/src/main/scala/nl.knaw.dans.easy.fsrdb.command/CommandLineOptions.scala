f142c43e3def8c9754bd523b037b411c6e292821
Paul Frölke <paul.frolke@dans.knaw.nl>
2019-12-02 13:16:44 +0100

    convert to maven multi-module

--- command/src/main/scala/nl.knaw.dans.easy.fsrdb.command/CommandLineOptions.scala
+++ command/src/main/scala/nl.knaw.dans.easy.fsrdb.command/CommandLineOptions.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.knaw.dans.easy.fsrdb
+package nl.knaw.dans.easy.fsrdb.command
 
 import java.io.File
 
